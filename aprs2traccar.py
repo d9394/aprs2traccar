1b9bbbd89d5b7ef967cf756860b87616aedeb3b6
d9394 <9394952@gmail.com>
2023-09-08 09:33:27 +0800

    Update aprs2traccar.py
    unfinished, must test by your self

--- aprs2traccar.py
+++ aprs2traccar.py
@@ -1,45 +1,103 @@
-#!/usr/bin/python
+#!/usr/bin/python3.8
 #coding=utf8
+
 from socket import * 
 import requests
-import time
 import re
 import uuid
 import threading
 import aprslib
-import datetime,time,json
-import sys
-if sys.getdefaultencoding() != 'utf8' :
-	reload(sys)
-	sys.setdefaultencoding('utf8')
-	
-def string2timestamp(strValue):
-	timeStamp=""
+import time,json
+from datetime import datetime,timedelta
+#import sys
+#if sys.getdefaultencoding() != 'utf8' :
+#	reload(sys)
+#	sys.setdefaultencoding('utf8')
+
+config={
+	"udp_car_server":("10.1.2.1:50025",),
+	"udp_aprs_server":("192.168.1.1:14580","aprs.hellocq.net:14580","202.141.176.2:14580",),
+	"relay_aprs_id":("BB1BB-5","BB2BB-7","BB3BB-6",),
+	"relay_car_id":("00000000","00000001","LM0C0174","00000059","00000072",)
+}
+
+def send_udp(type,msg):
+	nmSocket = socket(AF_INET,SOCK_DGRAM)
+	for ii in config[type]:
+		jj = ii.strip().split(":")
+		print("Send udp (%s) %s to %s" % (type, msg, jj))
+		ip_port = (gethostbyname(jj[0]), int(jj[1]))
+		nmSocket.sendto(msg.encode('utf-8'),ip_port)
+	nmSocket.close()
+
+def send_aprs_udp(msg):
+	#print(u'使用UDP转发')
+	nmSocket = socket(AF_INET,SOCK_DGRAM)
 	try:
-		d = datetime.datetime.strptime(strValue, "%Y%m%d%H%M%S")
-		t = d.timetuple()
-		timeStamp = int(time.mktime(t))
-		timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond))/1000000
-		print timeStamp
-		return timeStamp
-	except ValueError as e:
-		print e
-		print timeStamp
-		return timeStamp
+		#ip_port = (gethostbyname("aprs.hellocq.net"), 14580)
+		ip_port = (gethostbyname("srvr.aprs-is.net"), 8080)
+		nmSocket.sendto(msg.encode('utf-8'),ip_port)
+		print(u"Send APRS packet : %s" % (msg) )
+	except Exception as e:
+		print("Send APRS udp packet error %s" % e)
+	nmSocket.close()
+
+def send_aprs_tcp(msg):
+#	print(u'使用TCP转发')
+	tcp_client_socket = socket(AF_INET,SOCK_STREAM)
+	try:
+		tcp_client_socket.connect(("202.141.176.2",14580))
+		aprs_data=msg.split("\r\n")
+		for mm in aprs_data:
+#			print(u'发送数据：%s' % mm.encode('utf-8'))
+			tcp_client_socket.send(mm.encode('utf-8')+'\n')
+			recv_data = tcp_client_socket.recv(1024)
+			if not recv_data:
+				print(u"对方已离线。。")
+				break
+#			else:
+#				print(u"返回的消息为:",recv_data.decode('utf-8'))
+	except Exception as e:
+		print(u"连接上级APRS服务器出错：%s" % e)
+	tcp_client_socket.close()
+
+#def string2timestamp(strValue):
+#	timeStamp=""
+#	try:
+#		d = datetime.strptime(strValue, "%Y%m%d%H%M%S")
+#		t = d.timetuple()
+#		timeStamp = int(time.mktime(t))
+#		timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond))/1000000
+#		print timeStamp
+#	except ValueError as e:
+#		print("Change %s to timestamp error : %s" %(strValue,e))
+#		timeStamp=time.time()
+#	return timeStamp
 
 def car_udp():
 	mSocket = socket(AF_INET,SOCK_DGRAM)
 	mSocket.bind(("",50025)) 
 	while True:
-		revcData, (remoteHost, remotePort) = mSocket.recvfrom(1024)
-#		revcData="$SJHX,341200007E7E00007E7E020301803955352401161766210162090501010108191625132655351234567F12345F1"
-		revcData=revcData.decode("gb2312").strip()
-		print("Recv CAR UDP: %s, from %s:%s" % (revcData,remoteHost, remotePort))
-		if len(revcData)>0 :
-			GPS_Info = car_decode(revcData)
-#			print("Decode : %s " % GPS_Info)
+		recvData, (remoteHost, remotePort) = mSocket.recvfrom(1024)
+#		recvData="$SJHX,341200007E7E00007E7E020301803955352401161766210162090501010108191625132655351234567F12345F1"
+#		recvData="$SJHX,720000007E7E01007E7E02030100230531480113152627000000001811110956440609400000000000000000001"
+		mSocket.sendto("R".encode("UTF-8"),(remoteHost, remotePort))
+		recvData=recvData.strip()
+		print("Recv CAR UDP: %s, from %s:%s\n" % (recvData,remoteHost, remotePort))
+		if len(recvData)>0 :
+			try:
+				recvData = recvData.decode("gb2312")
+			except :
+				print("recvData decode error")
+			#可能car星历有问题，日期错乱，以当前日期时间替换
+			#recvData=recvData[:60] + datetime.datetime.now().strftime('%Y%m%d%H%M%S')[2:] + recvData[72:]
+			recvData=recvData[:60] + time.strftime('%Y%m%d',time.localtime(time.time()))[2:]+ recvData[66:]
+			GPS_Info = car_decode(recvData)
+			#print("Decode : %s " % GPS_Info)
 			if GPS_Info :
 				result = send_traccar(GPS_Info)
+				if remoteHost!="10.108.22.166" :
+					send_udp("udp_car_server", recvData)
 			else:
 				print("Decode error : %s" % GPS_Info)
 	mSocket.close()
@@ -48,26 +106,35 @@ def aprs_udp():
 	mSocket = socket(AF_INET,SOCK_DGRAM)
 	mSocket.bind(("",14580)) 
 	while True:
-		revcData, (remoteHost, remotePort) = mSocket.recvfrom(1024)
-		revcData=revcData.decode("gb2312").strip()
-		print("%s Recv APRS UDP: %s\n from %s:%s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), revcData,remoteHost, remotePort))
-		if len(revcData)>0 :
-			GPS_Info = aprs_decode(revcData)
-#			print("Decode : %s " % GPS_Info)
+		recvData, (remoteHost, remotePort) = mSocket.recvfrom(1024)
+		recvData=recvData.decode("gb2312").strip()
+		print("%s Recv APRS UDP: %s\n from %s:%s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), recvData,remoteHost, remotePort))
+		if len(recvData)>0 :
+			GPS_Info = aprs_decode(recvData)
+			print("Decode : %s " % GPS_Info)
 			if GPS_Info :
-				result = send_traccar(GPS_Info)
+				result = send_traccar(GPS_Info)				#转发GPS数据去traccar服务器
+#				send_udp("udp_car_server", recvData)
+#				if GPS_Info['id'][0]=='B' :
+				if GPS_Info['id'] in config['relay_aprs_id'] :
+#					send_aprs_tcp(recvData)				#转发aprs去202.141.176.2服务器
+					send_aprs_udp(recvData)
+#					pass
 			else:
 #				result = "APRS parsing result null"
-				print("APRS parsing result error : %s" % revcData)
+				print("APRS parsing result error : %s" % recvData)
+
 	mSocket.close()
 
 def aprs_decode(udp_packet):
 	k={}
 	aprs_data=udp_packet.split("\n")
 	if len(aprs_data)>1 :
-		aa=aprs_data[0].split(" ")
+#		aa=aprs_data[1].split(" ")
+#		print("APRS data split : %s " % aa)
 		try:
-			if aa[3]==str(aprslib.passcode(aa[1].encode("UTF-8"))) :
+#			if aa[3]==str(aprslib.passcode(aa[1].encode("UTF-8").upper())) :
+			if 1 :
 				try:
 					packet = aprslib.parse(aprs_data[1])
 				except (aprslib.ParseError, aprslib.UnknownFormat) as exp:
@@ -75,6 +142,7 @@ def aprs_decode(udp_packet):
 					print("Aprs packet %s " % udp_packet)
 					packet = None
 				else:
+					print("aprs decode info : %s" % packet)
 					k['id']=packet['from'].upper()
 					try:
 						k['lat']=packet['latitude']
@@ -93,15 +161,23 @@ def aprs_decode(udp_packet):
 					except:
 						k['hdop']=0
 					try:
-						k['timestamp']=packet['']
+						k['timestamp']=packet['timestamp']
 					except:
 						k['timestamp']=int(time.time())
 					try:
 						k['altitude']=packet['altitude']
 					except:
 						k['altitude']=0
-		except :
-			print("Aprs split : %s " % aa)
+					try:
+						k['gpsfixstatus']=packet['gpsfixstatus']
+					except:
+						pass
+					try:
+						k['format']=packet['format']
+					except:
+						pass
+		except Exception as e:
+			print("Aprs split : %s (error info : %s)" % (aa,e))
 	else:
 		print("Aprs packet : %s " % udp_packet)
 	return k
@@ -112,43 +188,55 @@ def car_decode(udp_packet):
 	for m in p.finditer(udp_packet) :
 		k = m.groupdict()
 	if len(k) > 0:
-		#{'hdop': '0905', 'gps_sign': '25', 'car_lock': '0', 'lon': '0116176621', 'id': '34120000', 'gps_lock': '8', 'car_bat': '132', 'timestamp': '010101081916', 'lat': '39553524', 'speed': '0162', 'car_status': '0000', 'car_temp': '65535'}
+		#{'hdop': u'0905', 'gps_sign': u'51', 'car_lock': u'0', 'lon': u'0116176621', 'id': u'72000000', 'gps_lock': u'8', 'car_bat': u'326', 'timestamp': u'200209119162', 'lat': u'39553524', 'speed': u'0162', 'car_status': u'0000', 'car_temp': u'55351'}
+
 		#细项转换
 		try:
 			temp=k['id']
 			k['id']=temp[6:8]+temp[4:6]+temp[2:4]+temp[0:2]
-			k['lon']=k['lon'][0:4] + "." + k['lon'][4:]
-			k['lat']=k['lat'][0:2] + "." + k['lat'][2:]
-			k['speed']=str(int(k['speed'])/10*1.852)
+			temp=k['lon']
+			k['lon']=float(temp[0:4]) + (float(temp[4:10])/10000/60)
+			temp=k['lat']
+			k['lat']=float(temp[0:2]) + (float(temp[2:8])/10000/60)
+			k['speed']=str(int(k['speed'])/10*1.852)		#原始上传速度为：节，需转换为公里/小时
 			k['hdop']=k['hdop'][0:3] + "." + k['hdop'][3:]
-			k['timestamp']=str(int(string2timestamp("20"+k['timestamp'])))
+			k_time = datetime.strptime("20"+k['timestamp'], "%Y%m%d%H%M%S") + timedelta(hours=8)
+			#k_time = datetime.strptime("20"+k['timestamp'], "%Y%m%d%H%M%S")
+			#timestamp为UTC时间，要转换为GMT+8时间
+			k['timestamp']=str(int(time.mktime(k_time.timetuple() )))
+			#CAR星历可能有问题，上报日期错误，直接取当前日期时间
+			#k_time=str(int(time.mktime(time.localtime())))
 			k['altitude']=0
-		except:
+		except Exception as e:
+			print("car decode error : %s\n%s\n" % (k,e))
 			k = None
 		return k
 	
 def send_traccar(msg):
-	Http_url = 'http://127.0.0.1:5055/?'
+	Http_url = u'http://127.0.0.1:5055/?'
 	for mm in msg:
+		if mm=="speed" :
+			msg[mm] = msg[mm] * 0.54		#根据traccar(OsmAnd)速度单位为：节
 		Http_url = Http_url + mm + "=" + str(msg[mm]) + "&"
-	Http_url = Http_url[:-1].decode('utf-8')
+	Http_url = Http_url[:-1].encode('utf-8').decode('utf-8')
 	t = threading.Thread(target=get_threading,args=(Http_url,), name=uuid.uuid1())
 	t.start()
 	return
 
 def get_threading(get_url):
 	request_result=""
-	print("%s, Request URL : %s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),get_url))
+	print("\n%s, Request URL : %s" % (time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())),get_url))
 	try:
 		req_session = requests.session()
-		revcData = req_session.get(get_url)
+		recvData = req_session.get(get_url)
 		time.sleep(15)
 		req_session.close()
 		request_result="OK"
-	except urllib2.HTTPError, e:
+#	except urllib2.HTTPError, e:
+	except Exception as e:
 		request_result=("Send Data to traccar Server Error : %s" % e)
+	print("Update traccar : %s" % request_result)
 	return request_result
-
 
 if __name__ == '__main__':
 	t1 = threading.Thread(target=car_udp, name='car_udp_server')  # 线程对象.
